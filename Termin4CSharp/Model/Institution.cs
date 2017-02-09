3e3a0a10256e7b8d45d6c5b14339180c8ae76d68
ebbakristiina <ebbakristiina@bitbucket>
2017-02-09 14:00:04 +0100

    Adding models from UML

---
+++ Termin4CSharp/Model/Institution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Termin4CSharp.Model
+{
+    class Institution
+    {
+        public string Name { get; set; }
+
+        public Institution(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
---
+++ Termin4CSharp/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Termin4CSharp.Model
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Phone_nbr { get; set; }
+        //List <int> roles 
+
+        public Person(string Name, int Id, string Email, string Phone_nbr)
+        {
+            this.Name = Name;
+            this.Id = Id;
+            this.Email = Email;
+            this.Phone_nbr = Phone_nbr;
+        }
+    }
+}

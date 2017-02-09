12529168baa4d4d85ce88bb136cb93b2db82f9d8
Baltzar <baltzar@hotmail.se>
2017-02-09 12:19:27 +0100

    Adding utils

---
+++ Termin4CSharp/Utils/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Termin4CSharp.Utils {
+    class Utils { 
+
+        public static Dictionary<string, object> GetAttributeInfo(Type t) {
+
+            MethodInfo[] mi = t.GetMethods();
+            foreach (MethodInfo m in mi)
+                Console.WriteLine(m.Name);
+
+
+            return null;
+        }
+
+    }
+}

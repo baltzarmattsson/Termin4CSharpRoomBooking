c5335804ad06b5f17b484d03a9acbce0020e5cb0
Baltzar <baltzar@hotmail.se>
2017-02-09 13:48:10 +0100

    Adding GetAttributeInfo in Utils class

--- Termin4CSharp/Utils/Utils.cs
+++ Termin4CSharp/Utils/Utils.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Termin4CSharp.Utils {
-    class Utils { 
+    class Utils {
 
-        public static void Main(string[] args) {
-            GetAttributeInfo("a".GetType());
+        public static Dictionary<string, object> GetAttributeInfo(Object paramObj) {
+            Dictionary<string, object> attributeValues = new Dictionary<string, object>();
+            Type t = paramObj.GetType();
+            var names = t.GetMembers()
+                        .Select(x => x.Name)
+                        .Where(x => !Regex.IsMatch(x, "([g|s]et)|(ToString|Equals|GetHashCode|GetType|.ctor)"));
+            //Console.WriteLine(string.Join(", ", names));
+
+            foreach (string attName in names) {
+                PropertyInfo pi = t.GetProperty(attName);
+                attributeValues[attName] = pi == null ? "" : pi.GetValue(paramObj, null);
+            }
+            return attributeValues;
         }
-
-        public static Dictionary<string, object> GetAttributeInfo(Type t) {
-
-            MethodInfo[] mi = t.GetMethods();
-            foreach (MethodInfo m in mi)
-                Console.WriteLine(m.Name);
-
-
-            return null;
-        }
-
     }
 }

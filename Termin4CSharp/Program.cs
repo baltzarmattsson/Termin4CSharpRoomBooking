6b32a431b0e45d4686b38bd4a54b334d8898baa6
Baltzar <baltzar@hotmail.se>
2017-02-14 12:44:00 +0100

    Some testing

--- Termin4CSharp/Program.cs
+++ Termin4CSharp/Program.cs
@@ -16,42 +16,42 @@ namespace Termin4CSharp {
         /// </summary>
         [STAThread]
         static void Main() {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Person p2 = new Person("Namnnnp2", "1337p2", "email@@p2", "+4600p2");
-            //Building b2 = new Building("Buildnameb2", "buildaddressb2", DateTime.Now, DateTime.Now);
-            //DAL dal = new DAL();
-            //IModel fetched;
-            //if (dal.Get(b2).Count > 0)
-            //    fetched = dal.Get(b2).First();
-            //else
-            //    fetched = b2;
-
-            //EditView ev = new EditView(fetched);
-            //EditViewController c = new EditViewController(ev);
-            //Application.Run(ev);
-
-            DAL dal = new DAL();
-            Building b = new Building("Buildname", "buildaddress", DateTime.Now, DateTime.Now);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Person p2 = new Person("Namnnnp2", "1337p2", "email@@p2", "+4600p2");
             Building b2 = new Building("Buildnameb2", "buildaddressb2", DateTime.Now, DateTime.Now);
-            Building b3 = new Building("Buildnameb3", "buildaddressb2", DateTime.Now, DateTime.Now);
-            Building b4 = new Building("Buildnameb4", "buildaddressb2", DateTime.Now, DateTime.Now);
-            dal.Remove(b);
-            dal.Remove(b2);
-            dal.Remove(b3);
-            dal.Remove(b4);
-            dal.Add(b);
-            dal.Add(b2);
-            dal.Add(b3);
-            dal.Add(b4);
-            Room r = new Room(b, "1337", 134141, "0", null);
-            Room r2 = new Room(b2, "1337r2", 13414122, "0", null);
-            dal.Add(r);
-            dal.Add(r2);
-            dal.Add(new Room(null, "test", 123123, "123", null));
+            DAL dal = new DAL();
+            IModel fetched;
+            if (dal.Get(b2).Count > 0)
+                fetched = dal.Get(b2).First();
+            else
+                fetched = b2;
 
-            Room asd = dal.Get(r).First() as Room;
-            Console.WriteLine(asd);
+            EditView ev = new EditView(fetched);
+            EditViewController c = new EditViewController(ev);
+            Application.Run(ev);
+
+            //DAL dal = new DAL();
+            //Building b = new Building("Buildname", "buildaddress", DateTime.Now, DateTime.Now);
+            //Building b2 = new Building("Buildnameb2", "buildaddressb2", DateTime.Now, DateTime.Now);
+            //Building b3 = new Building("Buildnameb3", "buildaddressb2", DateTime.Now, DateTime.Now);
+            //Building b4 = new Building("Buildnameb4", "buildaddressb2", DateTime.Now, DateTime.Now);
+            //dal.Remove(b);
+            //dal.Remove(b2);
+            //dal.Remove(b3);
+            //dal.Remove(b4);
+            //dal.Add(b);
+            //dal.Add(b2);
+            //dal.Add(b3);
+            //dal.Add(b4);
+            //Room r = new Room(b, "1337", 134141, "0", null);
+            //Room r2 = new Room(b2, "1337r2", 13414122, "0", null);
+            //dal.Add(r);
+            //dal.Add(r2);
+            //dal.Add(new Room(null, "test", 123123, "123", null));
+
+            //Room asd = dal.Get(r).First() as Room;
+            //Console.WriteLine(asd);
             /*
             //Connector.getConnection(); 
             Person p = new Person("Namnnn", "1337", "email@@", "+4600");

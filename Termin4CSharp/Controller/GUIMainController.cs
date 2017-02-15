1ab36dedbacd22380ecebb1b921674ce067ad6ea
Baltzar <baltzar@hotmail.se>
2017-02-15 12:59:00 +0100

    Adding guimaincontroller

---
+++ Termin4CSharp/Controller/GUIMainController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Termin4CSharp.Model;
+
+namespace Termin4CSharp.Controller {
+    class GUIMainController : IController {
+        public void NotifyExceptionToView() {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}

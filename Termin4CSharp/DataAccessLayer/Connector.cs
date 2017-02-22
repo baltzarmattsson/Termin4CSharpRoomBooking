9f2775277a0acc22e83d6de0dca569350ef53d74
ebbakristiina <ebbakristiina@bitbucket>
2017-02-22 11:57:48 +0100

    Changes

--- Termin4CSharp/DataAccessLayer/Connector.cs
+++ Termin4CSharp/DataAccessLayer/Connector.cs
@@ -19,16 +19,17 @@ namespace Termin4CSharp.DataAccessLayer
         //                                     "server= "   + SERVER   + ";" +
         //                                     "Integrated security=false;"  + // false since it's not dependant on SSIP
         //                                     "database="  + DATABASE + ";" + 
-        //                                     "connection timeout=30";
+        //                                     "connection timeout=30";      
 
         //Baltzar local
         private static readonly string URL = "Data Source=DESKTOP-STUECFJ;Initial Catalog=tempdb;Integrated Security=True";
+        private static readonly string URL2 = "Data Source=sql5030.smarterasp.net;Initial Catalog = DB_A15DA9_termin4; Persist Security Info=True;User ID = DB_A15DA9_termin4_admin; Password=Termin41337";
 
         public static SqlConnection GetConnection()
         {
             try
             {
-                SqlConnection conn = new SqlConnection(URL);
+                SqlConnection conn = new SqlConnection(URL2);
                 conn.Open();
                 //Console.WriteLine("Connection open!"); 
                 //conn.Close(); 
